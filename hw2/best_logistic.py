5b94661a3395eb8133b83d79bfc9a1bac1f2372b
unknown <user.name>
2017-10-26 21:45:19 +0800

    without report

---
+++ hw2/best_logistic.py
@@ -0,0 +1,266 @@
+import os, sys
+import numpy as np
+from random import shuffle
+import argparse
+from math import log, floor
+import pandas as pd
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import LSTM, Conv2D, MaxPooling2D, Flatten, Masking, Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam
+from keras.utils import np_utils
+from keras.models import load_model
+
+# If you wish to get the same shuffle result
+# np.random.seed(2401)
+
+def load_data(train_data_path, train_label_path, test_data_path):
+    X_train = pd.read_csv(train_data_path, sep=',', header=0)
+    """
+    X_train = X_train.drop(X_train.columns[64:], axis = 1)
+    X_train = X_train.drop(X_train.columns[53:59], axis = 1)
+    X_train = X_train.drop(X_train.columns[31:38], axis = 1)
+    X_train = X_train.drop(X_train.columns[2], axis = 1)
+    """
+    X_train = np.array(X_train.values)
+    Y_train = pd.read_csv(train_label_path, sep=',', header=0)
+    Y_train = np.array(Y_train.values)
+    X_test = pd.read_csv(test_data_path, sep=',', header=0)
+    """
+    X_test = X_test.drop(X_test.columns[64:], axis = 1)
+    X_test = X_test.drop(X_test.columns[53:59], axis = 1)
+    X_test = X_test.drop(X_test.columns[31:38], axis = 1)
+    X_test = X_test.drop(X_test.columns[2], axis = 1)
+    """
+    X_test = np.array(X_test.values)
+    
+    return (X_train, Y_train, X_test)
+
+def _shuffle(X, Y):
+    randomize = np.arange(len(X))
+    np.random.shuffle(randomize)
+    return (X[randomize], Y[randomize])
+
+def normalize(X_all, X_test):
+    # Feature normalization with train and test X
+    X_train_test = np.concatenate((X_all, X_test))
+    mu = (sum(X_train_test) / X_train_test.shape[0])
+    sigma = np.std(X_train_test, axis=0)
+    mu = np.tile(mu, (X_train_test.shape[0], 1))
+    sigma = np.tile(sigma, (X_train_test.shape[0], 1))
+    X_train_test_normed = (X_train_test - mu) / sigma
+
+    # Split to train, test again
+    X_all = X_train_test_normed[0:X_all.shape[0]]
+    X_test = X_train_test_normed[X_all.shape[0]:]
+    return X_all, X_test
+
+def split_valid_set(X_all, Y_all, percentage):
+    all_data_size = len(X_all)
+    valid_data_size = int(floor(all_data_size * percentage))
+
+    X_all, Y_all = _shuffle(X_all, Y_all)
+
+    X_train, Y_train = X_all[0:valid_data_size], Y_all[0:valid_data_size]
+    X_valid, Y_valid = X_all[valid_data_size:], Y_all[valid_data_size:]
+
+    return X_train, Y_train, X_valid, Y_valid
+
+def sigmoid(z):
+    res = 1 / (1.0 + np.exp(-z))
+    return np.clip(res, 1e-8, 1-(1e-8))
+
+def valid(w, b, X_valid, Y_valid):
+    valid_data_size = len(X_valid)
+
+    z = (np.dot(X_valid, np.transpose(w)) + b)
+    y = sigmoid(z)
+    y_ = np.around(y)
+    result = (np.squeeze(Y_valid) == y_)
+    print('Validation acc = %f' % (float(result.sum()) / valid_data_size))
+    return
+
+def train(X_all, Y_all, save_dir):
+
+    # Split a 10%-validation set from the training set
+    valid_set_percentage = 0.1
+    X_train, Y_train, X_valid, Y_valid = split_valid_set(X_all, Y_all, valid_set_percentage)
+
+    # Initiallize parameter, hyperparameter
+    w = np.zeros((X_train.shape[1],))
+    b = np.zeros((1,))
+    l_rate = 0.1
+    batch_size = 32
+    train_data_size = len(X_train)
+    step_num = int(floor(train_data_size / batch_size))
+    epoch_num = 10000
+    save_param_iter = 1000
+
+    # Start training
+    total_loss = 0.0
+    ada_w_grad = np.zeros((X_train.shape[1],))
+    ada_b_grad = np.zeros((1,))
+    for epoch in range(1, epoch_num):
+        """
+        # Do validation and parameter saving
+        if (epoch) % save_param_iter == 0:
+            print('=====Saving Param at epoch %d=====' % epoch)
+            if not os.path.exists(save_dir):
+                os.mkdir(save_dir)
+            np.savetxt(os.path.join(save_dir, 'w'), w)
+            np.savetxt(os.path.join(save_dir, 'b'), [b,])
+            print('epoch avg loss = %f' % (total_loss / (float(save_param_iter) * train_data_size)))
+            total_loss = 0.0
+            valid(w, b, X_valid, Y_valid)
+        """
+        # Random shuffle
+        X_train, Y_train = _shuffle(X_train, Y_train)
+
+        # Train with batch
+        for idx in range(step_num):
+            X = X_train[idx*batch_size:(idx+1)*batch_size]
+            Y = Y_train[idx*batch_size:(idx+1)*batch_size]
+
+            z = np.dot(X, np.transpose(w)) + b
+            y = sigmoid(z)
+
+            cross_entropy = -1 * (np.dot(np.squeeze(Y), np.log(y)) + np.dot((1 - np.squeeze(Y)), np.log(1 - y)))
+            total_loss += cross_entropy
+
+            w_grad = np.sum(-1 * X * (np.squeeze(Y) - y).reshape((batch_size,1)), axis=0)
+            b_grad = np.sum(-1 * (np.squeeze(Y) - y))
+            
+            ada_w_grad += w_grad**2
+            ada_b_grad += b_grad**2
+            ada_w = np.sqrt(ada_w_grad)
+            ada_b = np.sqrt(ada_b_grad)
+
+            # SGD updating parameters
+            w = w - l_rate * w_grad/ada_w
+            b = b - l_rate * b_grad/ada_b
+            
+        # Do validation and parameter saving
+        if (epoch) % save_param_iter == 0:
+            print('=====Saving Param at epoch %d=====' % epoch)
+            if not os.path.exists(save_dir):
+                os.mkdir(save_dir)
+            np.savetxt(os.path.join(save_dir, 'w'), w)
+            np.savetxt(os.path.join(save_dir, 'b'), [b,])
+            print('epoch avg loss = %f' % (total_loss / (float(save_param_iter) * train_data_size)))
+            total_loss = 0.0
+            valid(w, b, X_valid, Y_valid)
+
+    return
+
+def infer(X_test, save_dir, output_dir):
+    test_data_size = len(X_test)
+
+    # Load parameters
+    print('=====Loading Param from %s=====' % save_dir)
+    w = np.loadtxt(os.path.join(save_dir, 'w'))
+    b = np.loadtxt(os.path.join(save_dir, 'b'))
+
+    # predict
+    z = (np.dot(X_test, np.transpose(w)) + b)
+    y = sigmoid(z)
+    y_ = np.around(y)
+
+    print('=====Write output to %s =====' % output_dir)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    output_path = os.path.join(output_dir, 'log_prediction.csv')
+    with open(output_path, 'w') as f:
+        f.write('id,label\n')
+        for i, v in  enumerate(y_):
+            f.write('%d,%d\n' %(i+1, v))
+
+    return
+    
+def train_with_NN(X_all, Y_all):
+    Y_all = np_utils.to_categorical(Y_all)
+    model = Sequential()
+    model.add(Dense(
+                input_dim = X_all.shape[1],
+                units = 2,
+                activation = 'softmax'))
+    model.add(Dense(units = 20))
+    model.add(Dense(units = 20))
+    model.add(Dense(units = 20))
+    model.add(Dense(units = 20))
+    model.add(Dense(units = 20))
+    model.add(Dense(units = 20))
+    model.add(Dense(units = 20))
+    model.add(Dense(
+                units = 2,
+                activation = 'softmax'))                
+              
+    model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])                
+    #Starting trainning
+    model.fit(  X_all, 
+                Y_all, 
+                batch_size=32, 
+                epochs=10,
+                validation_split = 0.01)
+                
+    model.save("NN_model/model.h5")
+    
+    
+def infer_with_NN(X_test, output_dir):
+    model = load_model("NN_model/model.h5")
+    result = model.predict(X_test)
+    y = []
+    for i in range(result.shape[0]):
+        y.append(np.argmax(result[i]))
+    
+    with open(output_dir, 'w') as f:
+        f.write('id,label\n')
+        for i in  range(len(y)):
+            f.write('%d,%d\n' %(i+1, y[i]))    
+    
+
+def main(opts):
+    # Load feature and label
+    X_all, Y_all, X_test = load_data(opts.train_data_path, opts.train_label_path, opts.test_data_path)
+    
+    # Normalization
+    X_all, X_test = normalize(X_all, X_test)
+
+    # To train or to infer
+    if opts.train:
+        """train(X_all, Y_all, opts.save_dir)"""
+        train_with_NN(X_all, Y_all)
+    elif opts.infer:
+        """infer(X_test, opts.save_dir, opts.output_dir)"""
+        infer_with_NN(X_test, opts.output_dir)
+    else:
+        print("Error: Argument --train or --infer not found")
+    return
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Logistic Regression with Gradient Descent Method')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--train', action='store_true', default=False,
+                        dest='train', help='Input --train to Train')
+    group.add_argument('--infer', action='store_true',default=False,
+                        dest='infer', help='Input --infer to Infer')
+    parser.add_argument('--train_data_path', type=str,
+                        default='data/X_train', dest='train_data_path',
+                        help='Path to training data')
+    parser.add_argument('--train_label_path', type=str,
+                        default='data/Y_train', dest='train_label_path',
+                        help='Path to training data\'s label')
+    parser.add_argument('--test_data_path', type=str,
+                        default='data/X_test', dest='test_data_path',
+                        help='Path to testing data')
+    parser.add_argument('--save_dir', type=str,
+                        default='logistic_params/', dest='save_dir',
+                        help='Path to save the model parameters')
+    parser.add_argument('--output_dir', type=str,
+                        default='logistic_output/', dest='output_dir',
+                        help='Path to save the model parameters')
+    opts = parser.parse_args()
+    main(opts)
---
+++ hw2/hw2_best.sh
@@ -0,0 +1,1 @@
+python best_logistic.py --infer --train_data_path $3 --train_label_path $4 --test_data_path $5 --output_dir $6
